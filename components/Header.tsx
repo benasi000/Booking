15acb2f58324582d28a3ce212efa67d459030e57
benjamin mušić <benjaminmusic98@gmail.com>
2023-12-26 13:38:09 +0100

    BOOK[0005]: installing heroIcons and popovers to the project.. adding hamburger menu icon to the header when on mobile

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,13 @@
+"use client";
+import { Bars3Icon,XMarkIcon } from "@heroicons/react/16/solid";
 import  Link  from "next/link"
+import { useState } from "react";
 
 function Header() {
-  return <header className="bg-[#013B94]">
+ const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <header className="bg-[#013B94]">
      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" 
      aria-label="Global">
       <div className="flex lg:flex-1">
@@ -11,9 +17,20 @@ function Header() {
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Booking.com_Logo_Grey.svg/2560px-Booking.com_Logo_Grey.svg.png"/>
        </Link>
       </div>
+
+      <div className="flex lg:hidden">
+        <button type="button"
+        className="-m-2.5 inline-flex items-center justify-center
+        rounded-md p-2.5 text-white"
+        onClick={() => setMobileMenuOpen(true)}>
+          <span className="sr-only">Open main menu</span>
+          <Bars3Icon className="h-6 w-6" aria-hidden="true"/>
+        </button>
+      </div>
+
      </nav>
     </header>
-  
+  )
 }
 
-export default Header
+export default Header;
